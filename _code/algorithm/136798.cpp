c1b00e89f5e69bb9c8cd3e78151dedab3d7c63bb
yo-young <kimyj7331@gmail.com>
2022-11-22 20:47:00 +0900

    Update 136798.cpp

--- _code/algorithm/136798.cpp
+++ _code/algorithm/136798.cpp
@@ -9,9 +9,12 @@ unordered_map<int, int> numDiv;
 
 void storeDiv(int n) {
     vector<int> div;
+    // 약수의 제곱은 n보다 클 수 없다.
     for (int i=1; i*i<=n; i++) {
         if(n % i == 0) {
-            div.push_back(i);
+            div.push_back(i); // 약수 저장
+            // 약수 i를 저장하고 제곱이 되는 약수가 아니라면
+            // i를 곱해서 n이 될 수 있는 수 n/i도 저장한다
             if(n/i != i) div.push_back(n/i);
         }
     }
