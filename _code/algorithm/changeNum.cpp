50c1feb1dc2bda9c6abd81dc44603b2fe6e87ac9
yo-young <kimyj7331@gmail.com>
2023-02-08 21:17:25 +0900

    Update changeNum.cpp

--- _code/algorithm/changeNum.cpp
+++ _code/algorithm/changeNum.cpp
@@ -5,33 +5,38 @@
 
 using namespace std;
 
-int solution(int x, int y, int n) {
-        int cnt = 0;
-        unordered_set<int> curr, next;
-        curr.insert(x);
+int solution(int x, int y, int n)
+{
+    int cnt = 0;
+    unordered_set<int> curr, next;
+    curr.insert(x);
 
-        while (!curr.empty()) {
-            if (curr.count(y))
-                return cnt;
-            next.clear();
-            for (int val : curr) {
-                int pVal = val + n;
-                int dVal = val * 2;
-                int tVal = val * 3;
-                if (pVal <= y) next.insert(pVal);
-                if (dVal <= y) next.insert(dVal);
-                if (tVal <= y) next.insert(tVal);
-            }
-            curr = next;
-            cnt++;
+    while (!curr.empty())
+    {
+        if (curr.count(y))
+            return cnt;
+        next.clear();
+        for (int val : curr)
+        {
+            int pVal = val + n;
+            int dVal = val * 2;
+            int tVal = val * 3;
+            if (pVal <= y)
+                next.insert(pVal);
+            if (dVal <= y)
+                next.insert(dVal);
+            if (tVal <= y)
+                next.insert(tVal);
         }
-        return -1;
+        curr = next;
+        cnt++;
+    }
+    return -1;
 }
 
-int main() {
+int main()
+{
     int ans;
-
     ans = solution(1, 100000, 1);
-
-    cout << ans <<endl;
+    cout << ans << endl;
 }
