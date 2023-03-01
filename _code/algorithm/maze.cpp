e858f6286dcc907b8f5e518b9f3f088e32bccd1b
yo-young <kimyj7331@gmail.com>
2023-03-01 14:40:37 +0900

    Update maze.cpp

--- _code/algorithm/maze.cpp
+++ _code/algorithm/maze.cpp
@@ -5,55 +5,7 @@
 #include <cstring>
 
 using namespace std;
-// int answer = 0;
-// bool found = false;
-// void dfs(const int x,const int y, const vector<string>& maps, int cnt, char target)
-// {
-//     if(target =='L' && maps[x][y] == 'L' ||
-//        target =='E' && maps[x][y] == 'E') {
-//         answer = cnt;
-//         found = true;
-//         return;
-//     }
-//     //right, down, left, up
-//     ++cnt;
-//     auto temp = visitedMaps[x][y];
-//     visitedMaps[x][y] = 'X';
-//     if(found) return;
-//     if (checkBorder(x, y+1, maps) &&
-//         visitedMaps[x][y+1] != 'X') {
-//         dfs(x, y+1, maps, cnt, target);
-//     }
-//     if (checkBorder(x+1, y, maps) &&
-//         visitedMaps[x+1][y] != 'X') {
-//         dfs(x+1, y, maps, cnt, target);
-//     }
-//     if (checkBorder(x, y-1, maps) &&
-//         visitedMaps[x][y-1] != 'X') {
-//         dfs(x, y-1, maps, cnt, target);
-//     }
-//     if (checkBorder(x-1, y, maps) &&
-//         visitedMaps[x-1][y] != 'X') {
-//         dfs(x-1, y, maps, cnt, target);
-//     }
-//     visitedMaps[x][y] = temp;
-//     --cnt;
-// }
-// int solution(vector<string> maps) {
-//     visitedMaps = maps;
-//     //find S, L
-//     pair<int, int> S;
-//     pair<int, int> L;
-//     findSL(S, L, maps);
-//     dfs(S.first, S.second, maps, 0, 'L');
-//     if(answer == 0) return -1;
-//     int toL = answer;
-//     found = false;
-//     visitedMaps = maps;
-//     dfs(L.first, L.second, maps, answer, 'E');
-//     if(toL == answer) return -1;
-//     return answer;
-// }
+
 int visitedMaps[101][101] = {0,};
 pair<int, int> moveDir[4] = {{-1,0},{1,0},{0,-1},{0,1}};
 void findSL(pair<int, int>& S, pair<int, int>& L, const vector<string>& maps)
@@ -112,6 +64,10 @@ int bfs(priority_queue<Position>& pq, const vector<string>& maps, char dest) {
 }
 
 int solution(vector<string> maps) {
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(3);
+    v.push_back(2);
     pair<int, int> S;
     pair<int, int> L;
     findSL(S, L, maps);
@@ -122,13 +78,11 @@ int solution(vector<string> maps) {
     //up, down, left, right
     int t = bfs(pq, maps, 'L');
     if (t == -1) return t;
-    cout << "to L : " << t << endl;
     memset(visitedMaps, 0, sizeof(visitedMaps));
     pq = priority_queue<Position>();
     pq.push({L.first, L.second, t});
     visitedMaps[L.first][L.second] = 1;
     t = bfs(pq, maps, 'E');
-    cout << "to E : " << t << endl;
     return t;
 }
 
